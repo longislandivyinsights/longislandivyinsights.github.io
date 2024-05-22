a54a73e544428627ca544ce08b6652445f2c7f23
Christopher Prainito <christopherprainito@college.harvard.edu>
2024-05-22 11:45:08 -0400

    Fix navigation bar overflow

--- src/navigation.js
+++ src/navigation.js
@@ -122,6 +122,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    © ${new Date().getFullYear()}, CollegeCoaches · All rights reserved.
+    © ${new Date().getFullYear()} CollegeCoaches · All rights reserved.
   `,
 };
