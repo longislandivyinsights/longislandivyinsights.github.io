bacc8c2a85929e58c099fc2adec045480130a45b
Christopher Prainito <christopherprainito@college.harvard.edu>
2024-05-26 20:31:01 -0400

    Fix footer social links error

--- src/navigation.js
+++ src/navigation.js
@@ -89,11 +89,11 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   // TODO: Add social links
-  // socialLinks: [
-  //   { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-  //   { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-  //   { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-  // ],
+  socialLinks: [
+    //   { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    //   { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    //   { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+  ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="${thirtyTwo.src}" alt="CollegeCoaches logo" loading="lazy"></img>
     © ${new Date().getFullYear()} CollegeCoaches · All rights reserved.
